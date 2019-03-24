bf7a63495f240e606936d04bab83cbd06675dbfd
Nikhil Hira <nikhil@leancode.io>
2019-03-24 16:58:15 -0700

    add styled-components to Page and use context to pass theme

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d58038cac39e685cb9cd";
+/******/ 	var hotCurrentHash = "b0e0336c560cabb1a116";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
